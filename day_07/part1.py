aa7461c699c085d90f44e5686cc54c28b9533764
declanyewlim <declan.lim@deepgenomics.com>
2024-12-07 14:21:38 -0500

    day 7

---
+++ day_07/part1.py
@@ -0,0 +1,47 @@
+def check_possible(target, numbers):
+    """divide and conquer to check if possible"""
+    if len(numbers) == 2:
+        add = numbers[0] + numbers[1]
+        mul = numbers[0] * numbers[1]
+        if add == target or mul == target:
+            return True
+        else:
+            return False
+    else:
+        # perform one operation and recurse
+        first = numbers.pop(0)
+        # add
+        add_list = numbers.copy()
+        add_list[0] = first + add_list[0]
+
+        # multiply
+        mul_list = numbers.copy()
+        mul_list[0] = first * mul_list[0]
+        
+        return check_possible(target, add_list) or check_possible(target, mul_list)
+
+
+def part1():
+    with open("input.txt") as f:
+        lines = f.readlines()
+
+    calibrations = []
+    for line in lines:
+        line = line.strip()
+        target, numbers = line.split(": ")
+        target = int(target)
+        numbers = [int(x) for x in numbers.split()]
+        calibrations.append((target, numbers))
+
+
+    total = 0
+    for target, numbers in calibrations:
+        if check_possible(target, numbers):
+            total += target
+
+    return total
+
+
+if __name__ == "__main__":
+    print(part1())
+
