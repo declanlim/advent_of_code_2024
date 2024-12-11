7a47b81b6ca5e1fc228e6536106317d45d049044
declanyewlim <declan.lim@deepgenomics.com>
2024-12-11 10:43:25 -0500

    day 9 part 1

--- day_09/part1.py
+++ day_09/part1.py
@@ -1,37 +1,17 @@
-def generate_str(disk_map):
-    rep = ""
-    is_space = 0
-    id_num = 0
-
-    for char in disk_map:
-        if is_space:
-            rep += "." * int(char)
-            is_space = 1 - is_space
-        else:
-            rep += str(id_num) * int(char)
-            is_space = 1 - is_space
-            id_num += 1
-        
-    
-    return rep
-
-
-
 def part1():
     with open("input.txt") as f:
         line = f.readline().strip()
 
-
-    max_id = len(line) // 2
+    max_id = len(line) // 2  # maximum file ID in the representation
 
     # dictionary of file ID to position in the representation
     pos_dict = {i: [] for i in range(max_id + 1)}
 
-    cur_id = 0 # the file ID for use in the representation
-    cur_pos = 0 # the position in the line
-    repr_pos = 0 # the position in the representation
-    gaps = [] # the indices of the gaps (that should be filled)
-    gap_size = [] # the size of each gap
+    cur_id = 0  # the file ID for use in the representation
+    cur_pos = 0  # the position in the line
+    repr_pos = 0  # the position in the representation
+    gaps = []  # the indices of the gaps (that should be filled)
+    gap_size = []  # the size of each gap
 
     while cur_pos < len(line):
         for _ in range(int(line[cur_pos])):
@@ -50,20 +30,40 @@ def part1():
         cur_id += 1
         cur_pos += 1
 
+    # maximum number of chars in the representation
+    max_repr_pos = sum(int(j) for j in line)
+    cur_id = 0
+    tail_id = max_id
+    checksum = 0
 
-    a = ""
-    for (k, v), gap in zip(pos_dict.items(), gap_size):
-        a += str(k) * len(v)
-        a += "." * gap
+    i = 0  # current position in the representation, to be multiplied by cur_id
+    while i < max_repr_pos:
+        if not pos_dict[cur_id]:
+            cur_id += 1  # increase cur_id if the current ID is empty
+            if cur_id > max_id:
+                break
+            continue
 
-    # add the final id since there is one less gap
-    a += str(max_id) * len(pos_dict[max_id])
+        if i in pos_dict[cur_id]:
+            checksum += i * cur_id
+            pos_dict[cur_id].remove(i)
+            i += 1
+            continue
+        else:
+            assert i in gaps
+            # fill the gap with the tail ID and remove the gap
+            tmp = pos_dict[tail_id].pop(-1)
 
+            checksum += i * tail_id
 
-    print(gap_size)
-    print(a)
+            if not pos_dict[tail_id]:
+                tail_id -= 1
 
-    return gaps, pos_dict
+            gaps.remove(i)
+            gaps.append(tmp)
+            i += 1
+
+    return checksum
 
 
 if __name__ == "__main__":
